127d21594eba63ffad9fd00aeccb8668d6ac79f5
KishiTheMechanic <s.kishi@elsoul.nl>
2023-09-10 14:11:02 +0200

    build

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,4 +1,3 @@
 export { createDataRef, createFirestoreDataConverter, getCollectionItem as get, addCollectionItem as add, serverTimestamp, createCollectionRef, addMultipleCollectionItems as adds, queryCollectionItems as query, updateCollectionItem as update, deleteCollectionItem as delete, } from './lib';
 export type { QueryCondition } from './lib';
 export { Timestamp, FieldValue } from 'firebase/firestore';
-export * as client from './client';
