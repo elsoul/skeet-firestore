885567c0e4e84972751ee71d591836734d8ad2a6
KishiTheMechanic <s.kishi@elsoul.nl>
2023-09-10 12:51:36 +0200

    update: release v2.2.1

--- dist/cjs/lib/version.js
+++ dist/cjs/lib/version.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
-exports.VERSION = '2.2.0';
+exports.VERSION = '2.2.1';
 //# sourceMappingURL=version.js.map
